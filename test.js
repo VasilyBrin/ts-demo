b26d26f33bc7bfa34d7e49755c9cfa1dd51514e6
VasilyBrin <mail@profitdata.ru>
2021-07-01 15:04:39 +0300

    Enums

--- test.js
+++ test.js
@@ -1,2 +1,34 @@
 "use strict";
-var a = 5;
+// числовой enum
+// enum Direction {
+// 	Up = 1,
+// 	Down,
+// 	Left,
+// 	Right
+// }
+// строковый enum
+var Direction;
+(function (Direction) {
+    Direction["Up"] = "UP";
+    Direction["Down"] = "DOWN";
+    Direction["Left"] = "LEFT";
+    Direction["Right"] = "RIGHT";
+})(Direction || (Direction = {}));
+// гетерогенный enum
+var Decision;
+(function (Decision) {
+    Decision[Decision["Yes"] = 1] = "Yes";
+    Decision[Decision["No"] = calcEnum()] = "No";
+})(Decision || (Decision = {}));
+function calcEnum() {
+    return 2;
+}
+function runEnum(obj) { }
+runEnum(Direction);
+var Test;
+(function (Test) {
+    Test[Test["A"] = 0] = "A";
+})(Test || (Test = {}));
+var test = Test.A;
+var nameA = Test[test]; // A
+var c = 0 /* A */;
--- test.ts
+++ test.ts
@@ -1,16 +1,62 @@
-let c: 'test' = 'test';
+// числовой enum
+// enum Direction {
+// 	Up = 1,
+// 	Down,
+// 	Left,
+// 	Right
+// }
 
-type actionType = 'up' | 'down';
-
-function performAction(action: actionType | ComplexAction) {
-  switch (action) {
-    case 'down':
-      return -1;
-    case 'up':
-      return 1;
-  }
+// строковый enum
+enum Direction {
+  Up = 'UP',
+  Down = 'DOWN',
+  Left = 'LEFT',
+  Right = 'RIGHT',
 }
 
-interface ComplexAction {
-	s: string;
+// гетерогенный enum
+enum Decision {
+  Yes = 1,
+  No = calcEnum(),
+}
+
+function calcEnum() {
+  return 2;
+}
+
+function runEnum(obj: { Up: string }) {}
+
+runEnum(Direction);
+
+enum Test {
+  A,
+}
+
+let test = Test.A;
+let nameA = Test[test]; // A
+
+const enum ConstEnum {
+  A,
+  B,
+}
+
+let c = ConstEnum.A;
+
+enum Dice {
+  One = 1,
+  Two,
+  Tree,
+}
+
+function ruDice(dice: Dice) {
+  switch (dice) {
+    case Dice.One:
+      return 'один';
+    case Dice.Two:
+      return 'два';
+    case Dice.Tree:
+      return 'три';
+    default:
+      const a: never = dice;
+  }
 }
